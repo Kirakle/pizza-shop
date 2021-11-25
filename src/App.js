cba7337c3d9071d2a828c60f12f7d635b33395b9
Kirakle <83728623+Kirakle@users.noreply.github.com>
2021-11-25 08:07:22 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router';
 
-import { Header, Home } from './components/index'
+import { Header, Home, Cart } from './components/index'
 
 function App() {
+    const [pizzas, setPizzas] = useState([]);
+
+    let getData = async () => {
+        let result = await axios.get('http://localhost:3000/db.json');
+        setPizzas(result.data.pizzas);
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
 
     return (
         <div className="wrapper">
             <Header />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home pizzas={pizzas} />} />
+                <Route path="/cart" element={<Cart />} />
             </Routes>
         </div>
     )
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Header } from './Header/Header';
 export { default as Categories } from './Categories/Categories';
 export { default as Sort } from './Sort/Sort';
 export { default as Home } from './Home/Home';
+export { default as Cart } from './Cart/Cart';
+export { default as PizzaItem } from './PizzaItem/PizzaItem';
